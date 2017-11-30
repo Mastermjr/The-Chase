423e0976826d6b10362997e81291cea39e27811a
07734willy <piortau@gmail.com>
2017-11-30 00:19:26 -0500

    Fixed map generation commit

---
+++ unity/The-Chase/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        loadMap("Default.map");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void loadMap(string filename) {
+        var lines = File.ReadAllLines("./Assets/Maps/" + filename);
+        int xpos, ypos;
+        ypos = 0;
+        foreach (var line in lines) {
+            Debug.Log(line);
+            xpos = 0;
+            foreach (char c in line) {
+                if (c == '\n') {
+                    break;
+                }
+                string blockname;
+                switch (c) {
+                    case 'G':
+                        blockname = "Block_Grass";
+                        break;
+                    case 'S':
+                        blockname = "Block_Stone";
+                        break;
+                    case ' ':
+                        xpos++;
+                        continue;
+                    default:
+                        throw new UnityException("Block Not Found");
+                }
+                GameObject go = Instantiate(Resources.Load(blockname), new Vector3(xpos, ypos, 0), Quaternion.identity) as GameObject;
+                xpos++;
+            }
+            ypos--;
+        }
+    }
+}
---
+++ unity/The-Chase/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float movementSpeed = 80f;
+    public float jumpForce = 4000f;
+    public Transform feetPosition;
+    public float groundedRadius = 0.2f;
+    public LayerMask terrain;
+    
+    private bool rightFacing = true;
+    private bool isGrounded = false;
+    private bool doubleJump = true;
+    private Rigidbody2D rbody;
+
+    // Use this for initialization
+    void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        if ((isGrounded || doubleJump) && Input.GetKeyDown(KeyCode.W)) {
+            doubleJump = isGrounded;
+            rbody.AddForce(new Vector2(0, jumpForce));
+        }
+
+    }
+
+    void FixedUpdate() {
+        isGrounded = Physics2D.OverlapCircle(feetPosition.position, groundedRadius, terrain);
+
+        float moveDirection = Input.GetAxis("Horizontal");
+
+
+        rbody.velocity = new Vector2(moveDirection * movementSpeed, rbody.velocity.y);
+
+        if ((moveDirection != 0) && ((moveDirection > 0) == !rightFacing)) {
+            rightFacing = !rightFacing;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+
+    }
+    
+}
