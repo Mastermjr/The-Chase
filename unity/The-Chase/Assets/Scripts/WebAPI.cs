46b1940354c5ece897c43c5470c541e31724e3b3
Mastermjr <mjrwarrior@yahoo.com>
2017-12-06 22:35:16 -0500

    adding in webdev support

---
+++ unity/The-Chase/Assets/Scripts/WebAPI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class WebAPI : MonoBehaviour {
+
+    public static IEnumerator Get(string url)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        UnityEngine.Debug.Log("Error: ");
+        yield return www.SendWebRequest();
+        if (www.isNetworkError)
+        {
+            UnityEngine.Debug.Log("Error: " + www.error);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Downloaded: " + www.downloadHandler.text);
+            // byte[] results = www.downloadHandler.data;
+
+          //  ArticlesCollection article = JsonUtility.FromJson<ArticlesCollection>(www.downloadHandler.text);
+        }
+    }
+
+}
+
